61f05163d1b5583b36262b002226cf9b6f767038
Darwin Stalin Ramirez Supe <dramirez@mag.gob.ec>
2021-03-15 11:28:12 -0500

    quitado eureka

--- src/main/java/ec/gob/mag/central/catalogo/MicroCentralCatalogos.java
+++ src/main/java/ec/gob/mag/central/catalogo/MicroCentralCatalogos.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@EnableResourceServer
 @SpringBootApplication
 public class MicroCentralCatalogos extends SpringBootServletInitializer {
 	private static Class<MicroCentralCatalogos> applicationClass = MicroCentralCatalogos.class;
